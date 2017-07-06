ce7a1e58ed722244a8b81b4e95aa3744c1b28a4b
Etienne Gandrille <etienne.gandrille@cea.fr>
2017-07-06 15:37:40 +0200

    IdValidator allows '-' caracter in names

--- src/main/java/fr/cea/organicity/manager/domain/IdValidator.java
+++ src/main/java/fr/cea/organicity/manager/domain/IdValidator.java
@@ -5,24 +5,23 @@ import javax.validation.ConstraintValidatorContext;
 
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class IdValidator implements ConstraintValidator<ValidateName, String> {
-	
-    public void initialize(ValidateName itf) {
-    }
 
-    public boolean isValid(String value, ConstraintValidatorContext context) {
-    	if (value == null) {
-    		return false;
-    	}
-    	
-    	for (String token : value.split(":")) {
-    		if (!token.matches("[a-zA-Z0-9]{2,40}")) {
-    			return false;
-    		}
-    	}
-    	
-    	return true;
-    }
+	public void initialize(ValidateName itf) {
+	}
+
+	public boolean isValid(String value, ConstraintValidatorContext context) {
+		if (value == null) {
+			return false;
+		}
+
+		for (String token : value.split(":")) {
+			for (String subtoken : token.split("-"))
+				if (!subtoken.matches("[a-zA-Z0-9]{2,40}"))
+					return false;
+		}
+
+		return true;
+	}
 }
